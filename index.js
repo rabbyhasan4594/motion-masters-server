1ea256a51c75884a8ec98b136db4160e8f5531c1
rabbyhasan <rabbyhasan4594@gmail.com>
2023-06-12 02:02:26 +0600

    users add in mongodb

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const port = process.env.PORT || 5000
 
 
 //middleware
-app.use(cors());
+const corsOptions = {
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions));
 app.use(express.json());
 
 
@@ -17,58 +22,73 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-   await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        client.connect();
 
 
 
-    const classesCollection = client.db('motionMasters').collection('classes')
+        const classesCollection = client.db('motionMasters').collection('classes');
+        const usersCollection = client.db('motionMasters').collection('users');
+
+
+        // users related api
+
+
+       
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email }
+            const existingUser = await usersCollection.findOne(query);
+
+            if (existingUser) {
+                return res.send({ message: 'user already exists' })
+            }
+
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
+
+
+        app.get('/classesAndInstructors', async (req, res) => {
+            const result = await classesCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.get('/popular', async (req, res) => {
+            const result = await classesCollection.find({}).limit(6).toArray();
+            res.send(result);
+        })
 
 
 
-    app.get('/classes',async(req,res)=>{
-        const result =await classesCollection.find().toArray();
-        res.send(result);
-    })
-
-    app.get('/popular',async(req,res)=>{
-        const result =await classesCollection.find({}).limit(6).toArray();
-        res.send(result);
-    })
-
-    app.get('/instructors',async(req,res)=>{
-        const data =await classesCollection.find().toArray();
-        res.send(data);
-    })
 
 
-
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
 
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('Dancing start')
 })
-app.listen(port,() =>{
+app.listen(port, () => {
     console.log(`Dancing is running on port ${port}`)
 })
- 
