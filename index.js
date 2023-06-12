098f3c95783b1628e57e6545149e8e85ca6a5a8d
rabbyhasan <rabbyhasan4594@gmail.com>
2023-06-12 22:48:45 +0600

    get for selected class

--- index.js
+++ index.js
@@ -44,6 +44,17 @@ async function run() {
 
        //enroll api
 
+       app.get('/selected', async (req, res) => {
+        const email = req.query.email;
+  
+        if (!email) {
+          res.send([]);
+        }
+        const query = { email: email };
+        const result = await selectedCollection.find(query).toArray();
+        res.send(result);
+      });
+
        app.post('/selected', async (req, res) => {
         const item = req.body;
         console.log(item.selectedId )
