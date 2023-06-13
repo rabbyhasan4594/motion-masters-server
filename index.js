6297fc992670cfc0eb3a79b22e9b5e5c5e43234c
rabbyhasan <rabbyhasan4594@gmail.com>
2023-06-13 12:14:46 +0600

    verify admin and instructor

--- index.js
+++ index.js
@@ -120,7 +120,32 @@ async function run() {
             const result = await usersCollection.insertOne(user);
             res.send(result);
         });
+//verify admin 
+        app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+      
+            if (req.decoded.email !== email) {
+              res.send({ admin: false })
+            }
+      
+            const query = { email: email }
+            const user = await usersCollection.findOne(query);
+            const result = { admin: user?.role === 'admin' }
+            res.send(result);
+          });
 
+          app.get('/users/instructor/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+      
+            if (req.decoded.email !== email) {
+              res.send({ instructor: false })
+            }
+      
+            const query = { email: email }
+            const user = await usersCollection.findOne(query);
+            const result = { instructor: user?.role === 'instructor' }
+            res.send(result);
+          })
 
         app.patch('/users/admin/:id', async (req, res) => {
             const id = req.params.id;
