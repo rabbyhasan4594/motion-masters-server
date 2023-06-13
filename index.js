646b8a86e5538b107c00b1a41baa9d578f1fba60
rabbyhasan <rabbyhasan4594@gmail.com>
2023-06-13 11:42:39 +0600

    verify jwt added

--- index.js
+++ index.js
@@ -16,8 +16,21 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' });
+    }
+const token = authorization.split(' ')[1];
 
-
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2yyawyx.mongodb.net/?retryWrites=true&w=majority`;
@@ -41,6 +54,18 @@ async function run() {
         const classesCollection = client.db('motionMasters').collection('classes');
         const usersCollection = client.db('motionMasters').collection('users');
         const selectedCollection = client.db('motionMasters').collection('selected');
+
+
+
+
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2h' })
+      
+            res.send({ token })
+          })
+
+
 
 
         //enroll api
